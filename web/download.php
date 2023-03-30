782510c6345e2181ff23a717b0d923d6d376adc1
OCcminette <83664303+OCcminette@users.noreply.github.com>
2023-03-30 19:28:35 +0200

    Update download.php
    - Removing the need of path input 
    - Ensuring that downloads only take place inside of app

--- web/download.php
+++ web/download.php
@@ -1,7 +1,16 @@
 <?php
-	$filename = $_POST["archiveName"];
-	header("Content-type: application/octet-stream");
-	header("Content-disposition: attachment; filename=\"{$filename}\"");
-	readfile($_POST["path"] . "/" . $filename);
-	exit();
+        session_start();
+        $filename = $_POST["archiveName"];
+
+        $app_path = __DIR__."/sessions/";
+        $personnal_path = $_SERVER['REMOTE_ADDR'] . "-" . session_id();
+        $filePath = $app_path . $personnal_path . '/' . $filename;
+
+        $filePath = str_replace('..', '', $filePath);
+
+        header("Content-type: application/octet-stream");
+        header("Content-disposition: attachment; filename=\"{$filename}\"");
+        readfile($filePath);
+
+        exit();
 ?>
